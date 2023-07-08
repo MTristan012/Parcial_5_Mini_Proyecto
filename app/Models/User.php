ac971762a7cbb1b0ae3d5c99948550b1b4aca6c5
MTristan <53493044+MTristan012@users.noreply.github.com>
2023-07-08 12:02:30 -0600

    A2.6

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
         'password',
         'permission',
         'status',
+        'address',
+        'birthday',
+        'class',
+        'dni',
     ];
 
     /**
