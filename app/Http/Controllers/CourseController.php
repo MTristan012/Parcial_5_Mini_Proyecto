c221e41cec1e727d33976c9b15cbbdbb16604629
MTristan <53493044+MTristan012@users.noreply.github.com>
2023-07-09 15:38:09 -0600

    A3.1

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -19,84 +19,223 @@ class CourseController extends Controller
     public function update(Request $request)
     {
         // Verificar los valores de las variables
-        dd($request->all());
-        /*
-        try {
-            if ($request->adminClassesTeacher === NULL) {
+        //dd($request->all());
+        //Sin Maestro -> Sin Maestro
+        if ($request->adminClassesTeacher === NULL && $request->adminClassesOldTeacher === NULL) {
+            try {
                 $teacherID = NULL;
                 $teacher = NULL;
-                $queryCourses = "UPDATE courses SET class=?, teacherID=?, teacher=? WHERE id=?";
-                $bindingsCourses = [
+                $query = "UPDATE courses SET class=?, teacherID=?, teacher=? WHERE id=?";
+                $bindings = [
                     $request->adminClassesClass,
                     $teacherID,
                     $teacher,
                     $request->adminClassesID,
                 ];
+                $sql = DB::update($query, $bindings);
+
+                // Verificar el valor de $sql
+                //dd($sql);
+
+                if ($sql == 0) {
+                    $sql = 1;
+                }
+            } catch (\Throwable $th) {
+                // Verificar el error
+                //dd($th);
+                $sql = 0;
+            }
+
+            if ($sql == true) {
+                return back()->with("Correct", "Information Updated");
             } else {
-                $adminTeacherInfo = explode(",", $request->adminClassesTeacher);
-                $adminTeacherID = $adminTeacherInfo[0];
-                $adminTeacher = $adminTeacherInfo[1];
-                $queryCourses = "UPDATE courses SET class=?, teacherID=?, teacher=? WHERE id=?";
-                $bindingsCourses = [
+                return back()->with("Incorrect", "Error");
+            }
+            //Nuevo Maestro -> Sin Maestro
+            
+        } else if ($request->adminClassesTeacher != NULL && $request->adminClassesOldTeacher === NULL) {
+            try{
+                $teacherInfo = explode(",", $request->adminClassesTeacher);
+                $teacherID = $teacherInfo[0];
+                $teacher = $teacherInfo[1];
+                $query = "UPDATE courses SET class=?, teacherID=?, teacher=? WHERE id=?";
+                $queryUsers = "UPDATE users SET class=? WHERE id=?";
+                $bindings = [
                     $request->adminClassesClass,
-                    $adminTeacherID,
-                    $adminTeacher,
+                    $teacherID,
+                    $teacher,
                     $request->adminClassesID,
                 ];
+                $bindingsUsers = [
+                    $request->adminClassesClass,
+                    $teacherID,
+                ];
+
+                $sql = DB::update($query, $bindings);
+                $sqlUsers = DB::update($queryUsers, $bindingsUsers);
+
+                // Verificar el valor de $sql
+                //dd($sql);
+                //dd($sqlUsers);
+
+                if ($sql == 0 && $sqlUsers) {
+                    $sql = 1;
+                    $sqlUsers = 1;
+                }
+
+            } catch (\Throwable $th) {
+                // Verificar el error
+                //dd($th);
+                $sql = 0;
+                $sqlUsers = 0;
             }
 
-            if ($request->adminClassesOldTeacher != NULL && $request->adminClassesTeacher === NULL) {
-                $userInfo = explode(",", $request->adminClassesOldTeacher);
-                $userID = $userInfo[0];
-                $userClass = NULL;
-                $query = "UPDATE users SET class=?, WHERE id=?";
+            if ($sql == true && $sqlUsers == true) {
+                return back()->with("Correct", "Information Updated");
+            } else {
+                return back()->with("Incorrect", "Error");
+            }
+            //Nuevo Maestro -> Viejo Maestro
+        } else if ($request->adminClassesTeacher != NULL && $request->adminClassesOldTeacher != NULL) {
+            if ($request->adminClassesTeacher == $request->adminClassesOldTeacher) {
+                try {
+                    $teacherInfo = explode(",", $request->adminClassesTeacher);
+                    $teacherID = $teacherInfo[0];
+                    $teacher = $teacherInfo[1];
+                    $query = "UPDATE courses SET class=?, teacherID=?, teacher=? WHERE id=?";
+                    $queryUsers = "UPDATE users SET class=? WHERE id=?";
+                    $bindings = [
+                        $request->adminClassesClass,
+                        $teacherID,
+                        $teacher,
+                        $request->adminClassesID,
+                    ];
+                    $bindingsUsers = [
+                        $request->adminClassesClass,
+                        $teacherID,
+                    ];
+
+                    $sql = DB::update($query, $bindings);
+                    $sqlUsers = DB::update($queryUsers, $bindingsUsers);
+
+                    // Verificar el valor de $sql
+                    //dd($sql);
+                    //dd($sqlUsers);
+
+                    if ($sql == 0 && $sqlUsers) {
+                        $sql = 1;
+                        $sqlUsers = 1;
+                    }
+                } catch (\Throwable $th) {
+                    // Verificar el error
+                    //dd($th);
+                    $sql = 0;
+                    $sqlUsers = 0;
+                }
+
+                if ($sql == true && $sqlUsers == true) {
+                    return back()->with("Correct", "Information Updated");
+                } else {
+                    return back()->with("Incorrect", "Error");
+                }
+            } else {
+                try{
+                    $oldTeacherInfo = explode(",", $request->adminClassesOldTeacher);
+                    $oldTeacherID = $oldTeacherInfo[0];
+                    $oldTeacherClass = NULL;
+                    $teacherInfo = explode(",", $request->adminClassesTeacher);
+                    $teacherID = $teacherInfo[0];
+                    $teacher = $teacherInfo[1];
+                    $query = "UPDATE courses SET class=?, teacherID=?, teacher=? WHERE id=?";
+                    $queryUsers = "UPDATE users SET class=? WHERE id=?";
+                    $queryOldUsers = "UPDATE users SET class=? WHERE id=?";
+                    $bindings = [
+                        $request->adminClassesClass,
+                        $teacherID,
+                        $teacher,
+                        $request->adminClassesID,
+                    ];
+                    $bindingsUsers = [
+                        $request->adminClassesClass,
+                        $teacherID,
+                    ];
+                    $bindingsOldUsers = [
+                        $oldTeacherClass,
+                        $oldTeacherID,
+                    ];
+
+                    $sql = DB::update($query, $bindings);
+                    $sqlUsers = DB::update($queryUsers, $bindingsUsers);
+                    $sqlOldUsers = DB::update($queryOldUsers, $bindingsOldUsers);
+
+                    // Verificar el valor de $sql
+                    //dd($sql);
+                    //dd($sqlUsers);
+                    //dd($sqlOldUsers);
+
+                    if ($sql == 0 && $sqlUsers && $sqlOldUsers) {
+                        $sql = 1;
+                        $sqlUsers = 1;
+                        $sqlOldUsers = 1;
+                    }
+                } catch (\Throwable $th) {
+                    // Verificar el error
+                    //dd($th);
+                    $sql = 0;
+                    $sqlUsers = 0;
+                    $sqlOldUsers = 0;
+                }
+
+                if ($sql == true && $sqlUsers == true && $sqlOldUsers == true) {
+                    return back()->with("Correct", "Information Updated");
+                } else {
+                    return back()->with("Incorrect", "Error");
+                }
+            }
+            //Sin Maestro -> Viejo Maestro
+        } else if ($request->adminClassesTeacher === NULL && $request->adminClassesOldTeacher != NULL) {
+            try {
+                $oldTeacherInfo = explode(",", $request->adminClassesOldTeacher);
+                $oldTeacherID = $oldTeacherInfo[0];
+                $teacherID = NULL;
+                $teacher = NULL;
+                $class = NULL;
+                $query = "UPDATE courses SET class=?, teacherID=?, teacher=? WHERE id=?";
+                $queryUsers = "UPDATE users SET class=? WHERE id=?";
                 $bindings = [
-                    $userClass,
-                    $userID,
+                    $request->adminClassesClass,
+                    $teacherID,
+                    $teacher,
+                    $request->adminClassesID,
                 ];
-            } else if ($request->adminClassesOldTeacher != NULL && $request->adminClassesTeacher != NULL && $request->adminClassesOldTeacher != $request->adminClassesTeacher){
-                $userInfo = explode(",", $request->adminClassesOldTeacher);
-                $userID = $userInfo[0];
-                $userClass = NULL;
-                $userNewInfo = explode(",", $request->adminClassesTeacher);
-                $userNewID = $userNewInfo[0];
-                $userNewClass = $request->adminClassesClass;
-                $query = "UPDATE users SET class=?, WHERE id=?";
-                $bindings = [
-                    $userClass,
-                    $userID,
+                $bindingsUsers = [
+                    $class,
+                    $oldTeacherID,
                 ];
-                $queryNew = "UPDATE users SET class=?, WHERE id=?";
-                $bindingsNew = [
-                    $userNewClass,
-                    $userNewID,
-                ];
+
+                $sql = DB::update($query, $bindings);
+                $sqlUsers = DB::update($queryUsers, $bindingsUsers);
+
+                // Verificar el valor de $sql
+                //dd($sql);
+                //dd($sqlUsers);
+
+                if ($sql == 0 && $sqlUsers) {
+                    $sql = 1;
+                    $sqlUsers = 1;
+                }
+            } catch (\Throwable $th) {
+                // Verificar el error
+                //dd($th);
+                $sql = 0;
+                $sqlUsers = 0;
             }
 
-            
-            $sql = DB::update($query, $bindings);
-            $sqlCourses = DB::update($queryCourses, $bindingsCourses);
-
-            // Verificar el valor de $sql
-            //dd($sql);
-            //dd($sqlCourses);
-
-            if ($sql == 0 && $sqlCourses == 0) {
-                $sql = 1;
-                $sqlCourses = 1;
+            if ($sql == true && $sqlUsers == true) {
+                return back()->with("Correct", "Information Updated");
+            } else {
+                return back()->with("Incorrect", "Error");
             }
-        } catch (\Throwable $th) {
-            // Verificar el error
-            //dd($th);
-            $sql = 0;
-            $sqlCourses = 1;
         }
-
-        if ($sql == true && $sqlCourses == true) {
-            return back()->with("Correct", "Information Updated");
-        } else {
-            return back()->with("Incorrect", "Error");
-        }
-        */
     }
 }
