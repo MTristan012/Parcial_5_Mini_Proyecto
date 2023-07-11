9e91238e40b8048c56bb9a7a1880c50ff178df25
MTristan <53493044+MTristan012@users.noreply.github.com>
2023-07-11 11:18:36 -0600

    A5.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Course;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $userAdmin = new User();
+        $userAdmin->name = "Administrator";
+        $userAdmin->email = "admin@admin";
+        $userAdmin->password = Hash::make('password');
+        $userAdmin->permission = 1;
+        $userAdmin->status = 1;
+        $userAdmin->save();
+
+        $userTeacher = new User();
+        $userTeacher->name = "Michel P";
+        $userTeacher->email = "teacher@teacher";
+        $userTeacher->password = Hash::make('password');
+        $userTeacher->permission = 2;
+        $$userTeacher->status = 1;
+        $userTeacher->save();
+
+        $userStudent = new User();
+        $userStudent->name = "Tristan A";
+        $userStudent->email = "student@student";
+        $userStudent->password = Hash::make('password');
+        $userStudent->permission = 3;
+        $userStudent->status = 1;
+        $userStudent->save();
+
+        $user = new User();
+        $user->name = "User";
+        $user->email = "user@user";
+        $user->password = Hash::make('password');
+        $user->permission = 1;
+        $user->status = 0;
+        $user->save();
+
+        $course1 = new Course();
+        $course1->class = "Spanish";
+        $course1->save();
+
+        $course2 = new Course();
+        $course2->class = "Italian";
+        $course2->save();
+
+        $course3 = new Course();
+        $course3->class = "Russian";
+        $course3->save();
     }
 }
